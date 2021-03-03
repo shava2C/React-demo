66b89247029ba2e705bb146334954443b4d9ef82
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-03 20:31:49 +0900

    要素の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
 export const App = () => {
-  return <p>hoge</p>;
+  return (
+    <>
+      <div>
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTodo</p>
+        <ul>
+          <div>
+            <li>hoge</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTodo</p>
+        <ul>
+          <div>
+            <li>hoge</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
