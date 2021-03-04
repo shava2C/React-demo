61075913d9c7fd788fcfd40ffc956e647c6dbf11
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-03-04 13:06:42 +0900

    変なエラーが出る件

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/inputTodo";
+import { IncompleatTodos } from "./components/incompleatTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
@@ -48,20 +49,11 @@ export const App = () => {
         onChange={onChangeTodoText}
         onClick={onClickAdd}
       />
-      <div className="incompleat-area">
-        <p className="title">未完了のTodo</p>
-        <ul>
-          {incompleatTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <IncompleatTodos
+        todos={IncompleatTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="compleat-area">
         <p className="title">完了のTodo</p>
         <ul>
